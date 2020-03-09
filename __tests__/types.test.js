4faf525207d50f5be7f73d5ecd5968830eb72d73
Nathan Martel <nathanmartel@gmail.com>
2020-03-09 14:14:18 -0700

    basic 'is' functions added

--- __tests__/types.test.js
+++ __tests__/types.test.js
@@ -1,12 +1,15 @@
 const {
   isNumber,
+  isString,
+  isBoolean,
   castToNumber,
   getCaster
 } = require('../lib/types.js');
 
+// isNumber
 describe('validator module', () => {
   describe('basic validation', () => {
-    it('properly tells if a value is a numbers', () => {
+    it('properly tells if a value is a number', () => {
       expect(isNumber(3)).toBeTruthy();
       expect(isNumber('hi')).toBeFalsy();
       expect(isNumber([])).toBeFalsy();
@@ -15,6 +18,33 @@ describe('validator module', () => {
       expect(isNumber(true)).toBeFalsy();
     });
   });
+
+  // isString
+  describe('basic validation', () => {
+    it('properly tells if a value is a string', () => {
+      expect(isString(3)).toBeFalsy();
+      expect(isString('hi')).toBeTruthy();
+      expect(isString([])).toBeFalsy();
+      expect(isString({})).toBeFalsy();
+      expect(isString(() => {})).toBeFalsy();
+      expect(isString(true)).toBeFalsy();
+    });
+  });
+
+  // isBoolean
+  describe('basic validation', () => {
+    it('properly tells if a value is a string', () => {
+      expect(isBoolean(3)).toBeFalsy();
+      expect(isBoolean(1)).toBeFalsy();
+      expect(isBoolean(0)).toBeFalsy();
+      expect(isBoolean('hi')).toBeFalsy();
+      expect(isBoolean([])).toBeFalsy();
+      expect(isBoolean({})).toBeFalsy();
+      expect(isBoolean(() => {})).toBeFalsy();
+      expect(isBoolean(true)).toBeTruthy();
+    });
+  });
+
 
   describe('casters', () => {
     it('can cast values to a number', () => {
