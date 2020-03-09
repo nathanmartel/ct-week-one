8764b0b2f652607c3688e40e858754143ec7b348
Nathan Martel <nathanmartel@gmail.com>
2020-03-09 14:51:37 -0700

    array and object 'is' functions added

--- __tests__/types.test.js
+++ __tests__/types.test.js
@@ -2,6 +2,8 @@ const {
   isNumber,
   isString,
   isBoolean,
+  isArray,
+  isObject,
   castToNumber,
   getCaster
 } = require('../lib/types.js');
@@ -13,9 +15,12 @@ describe('validator module', () => {
       expect(isNumber(3)).toBeTruthy();
       expect(isNumber('hi')).toBeFalsy();
       expect(isNumber([])).toBeFalsy();
+      expect(isNumber([1, 2])).toBeFalsy();
       expect(isNumber({})).toBeFalsy();
+      expect(isNumber({ foo: 'bar' })).toBeFalsy();
       expect(isNumber(() => {})).toBeFalsy();
       expect(isNumber(true)).toBeFalsy();
+      expect(isNumber(null)).toBeFalsy();
     });
   });
 
@@ -24,24 +29,66 @@ describe('validator module', () => {
     it('properly tells if a value is a string', () => {
       expect(isString(3)).toBeFalsy();
       expect(isString('hi')).toBeTruthy();
+      expect(isString('')).toBeTruthy();
+      expect(isString([1, 2])).toBeFalsy();
       expect(isString([])).toBeFalsy();
+      expect(isString({ foo: 'bar' })).toBeFalsy();
       expect(isString({})).toBeFalsy();
       expect(isString(() => {})).toBeFalsy();
       expect(isString(true)).toBeFalsy();
+      expect(isString(null)).toBeFalsy();
     });
   });
 
   // isBoolean
   describe('basic validation', () => {
-    it('properly tells if a value is a string', () => {
+    it('properly tells if a value is a boolean', () => {
       expect(isBoolean(3)).toBeFalsy();
       expect(isBoolean(1)).toBeFalsy();
       expect(isBoolean(0)).toBeFalsy();
       expect(isBoolean('hi')).toBeFalsy();
+      expect(isBoolean('')).toBeFalsy();
+      expect(isBoolean([1, 2])).toBeFalsy();
       expect(isBoolean([])).toBeFalsy();
+      expect(isBoolean({ foo: 'bar' })).toBeFalsy();
       expect(isBoolean({})).toBeFalsy();
       expect(isBoolean(() => {})).toBeFalsy();
       expect(isBoolean(true)).toBeTruthy();
+      expect(isBoolean(null)).toBeFalsy();
+    });
+  });
+
+  // isArray
+  describe('basic validation', () => {
+    it('properly tells if a value is an array', () => {
+      expect(isArray(3)).toBeFalsy();
+      expect(isArray('hi')).toBeFalsy();
+      expect(isArray('')).toBeFalsy();
+      expect(isArray([1, 2])).toBeTruthy();
+      expect(isArray([])).toBeTruthy();
+      expect(isArray({ foo: 'bar' })).toBeFalsy();
+      expect(isArray({})).toBeFalsy();
+      expect(isArray(() => {})).toBeFalsy();
+      expect(isArray(true)).toBeFalsy();
+      expect(isArray(null)).toBeFalsy();
+    });
+  });
+
+  // isObject
+  describe('basic validation', () => {
+    it('properly tells if a value is an object', () => {
+      expect(isObject(3)).toBeFalsy();
+      expect(isObject('hi')).toBeFalsy();
+      expect(isObject('')).toBeFalsy();
+      // Arrays are not primitives, therefore they are objects
+      expect(isObject([1, 2])).toBeTruthy();
+      expect(isObject([])).toBeTruthy();
+      expect(isObject({ foo: 'bar' })).toBeTruthy();
+      expect(isObject({})).toBeTruthy();
+      // Functions are objects
+      expect(isObject(() => {})).toBeTruthy();
+      expect(isObject(true)).toBeFalsy();
+      expect(isObject(null)).toBeFalsy();
     });
   });
 
