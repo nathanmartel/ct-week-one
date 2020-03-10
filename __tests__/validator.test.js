a2f6cca51bb439b20f991ceac0657da9c5a2ff47
Nathan Martel <nathanmartel@gmail.com>
2020-03-10 16:41:30 -0700

    Starting to pass

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -1,27 +1,26 @@
-const {
-  Validator,
-} = require('../lib/validator.js');
-
-const nameValidator = new Validator('name', String, true);
-
-const dog = {
-  name: 'spot',
-  age: '5',
-  weight: '20 lbs'
-};
-
-const god = {
-  namey: 'tops',
-  age: '95',
-  weight: '02 lbs'
-};
+const Validator = require('../lib/Validator.js');
 
 
 describe('validator module', () => {
   describe('nameValidator validation', () => {
+
+    const nameValidator = new Validator('name', { type: String, required: true });
+
+    const dog = {
+      name: 'spot',
+      age: '5',
+      weight: '20 lbs'
+    };
+
+    const god = {
+      name: 'tops',
+      age: '95',
+      weight: '02 lbs'
+    };
+
+
     it('properly returns or throws', () => {
       expect(nameValidator.validate(dog)).toMatch('spot');
-      expect(nameValidator.validate(god)).toMatch('tops');
       expect(nameValidator.validate(god)).toMatch('tops');
     });
 
