21d137b47368ae0150482dabdae45ad94afda808
Nathan Martel <nathanmartel@gmail.com>
2020-03-11 13:53:30 -0700

    bones in place

---
+++ __tests__/file-system.test.js
@@ -0,0 +1,29 @@
+const {
+  mkdirp,
+  writeJSON,
+  readJSON,
+  readDirectoryJSON,
+  updateJSON,
+  deleteFile
+} = require('../lib/file-system.js');
+
+describe('File System module', () => {
+  it('Should make a directory and all parent directories', () => {
+  });
+
+  it('Should write an object to a file', () => {
+  });
+
+  it('Should read an object from a file', () => {
+  });
+
+  it('Should read all files in a directory as objects', () => {
+  });
+
+  it('Should update a files JSON', () => {
+  });
+
+  it('Should delete a file', () => {
+  });
+
+});
