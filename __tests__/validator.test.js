58d3292182237105e30182bd7c1c5c7d3348845a
Nathan Martel <nathanmartel@gmail.com>
2020-03-10 17:37:48 -0700

    more passing, more failing

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -5,29 +5,82 @@ describe('validator module', () => {
   describe('nameValidator validation', () => {
 
     const nameValidator = new Validator('name', { type: String, required: true });
+    const ageValidator = new Validator('age', { type: Number, required: true });
+    const weightValidator = new Validator('weight', { type: String, required: false });
 
-    const dog = {
-      name: 'spot',
-      age: '5',
-      weight: '20 lbs'
-    };
+    it('1. Throws an error on a required key that is missing', () => {
+      const dog = {
+        age: 5,
+        weight: '20 lbs'
+      };
+      expect(nameValidator.validate(dog)).toThrowError('Missing required key >>name<<');
+    });    
 
-    const god = {
-      name: 'tops',
-      age: '95',
-      weight: '02 lbs'
-    };
-
-
-    it('properly returns or throws', () => {
-      expect(nameValidator.validate(dog)).toMatch('spot');
-      expect(nameValidator.validate(god)).toMatch('tops');
+    it('2a. Validates a required key of wrong type that is castable', () => {
+      const dog = {
+        name: 55555,
+        age: '5',
+        weight: '20 lbs'
+      };  
+      expect(nameValidator.validate(dog)).toMatch('55555');
+      expect(ageValidator.validate(dog)).toEqual(5);
     });
 
-    // it('throws if value is not castable to a number', () => {
-    //   expect(() => castToNumber('hi')).toThrowErrorMatchingSnapshot();
-    // });
-    
+    it('2b. Throws an error on a required key of wrong type that is not castable', () => {
+      const dog = {
+        name: null,
+        age: 5,
+        weight: '20 lbs'
+      };
+      expect(nameValidator.validate(dog)).toThrowError('Cannot cast >>null<< to String');
+    });    
+
+    it('3. Validates a required key of right type', () => {
+      const dog = {
+        name: 'spot',
+        age: 5,
+        weight: '20 lbs'
+      };  
+      expect(nameValidator.validate(dog)).toMatch('spot');
+      expect(ageValidator.validate(dog)).toEqual(5);
+    });
+
+    it('4. Validates a non-required key that is missing', () => {
+      const dog = {
+        name: 'spot',
+        age: 5,
+      };
+      expect(weightValidator.validate(dog)).toEqual(null);
+    });    
+
+    it('5a. Validates a non-required key of wrong type that is castable', () => {
+      const dog = {
+        name: 'spot',
+        age: 5,
+        weight: 20
+      };
+      expect(weightValidator.validate(dog)).toMatch('2');
+    });    
+
+    it('5b. Throws an error on a non-required key of wrong type that is not castable', () => {
+      const dog = {
+        name: 'spot',
+        age: 5,
+        weight: function(a) { return a + 2; }
+      };
+      expect(weightValidator.validate(dog)).toThrowError('Cannot cast >>null<< to String');
+    });    
+
+    it('6. Validates a non-required key of right type', () => {
+      const dog = {
+        name: 'spot',
+        age: 5,
+        weight: '20 lbs'
+      };
+      expect(weightValidator.validate(dog)).toMatch('20 lbs');
+    });    
+
+
   });
 });
 
